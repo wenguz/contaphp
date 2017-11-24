cd9c50e7939cbbc2ac3b1e35a686c19d472647ef
Cinthia <cinthia.alvarezs3@gmail.com>
2017-11-24 15:01:06 -0400

    se agrego el recuperar contraseña no es funcional aun por el tiempo. Deberia mandar una notificacion al admin

---
+++ sesion_password.php
@@ -0,0 +1,60 @@
+<?php
+$usuario = $_POST["usuario"];
+$password = $_POST["contra"];
+$intentos = 0;
+include('conexion.php');
+$result = mysqli_query($con,"SELECT a.cargo as cargo from empleado a, empleado_usuario b where a.id_empleado=b.id_empleado and  b.user='$usuario' and b.password='$password'");
+
+
+
+
+while ($intentos<=3)
+{
+	if($row = mysqli_fetch_array($result))
+	{
+	 	if($row["cargo"] == 'ADMINISTRADOR')
+		{
+			session_start();
+			$_SESSION['usuario'] = $usuario;
+			header("Location: principal_administrador.php");
+		}
+		else
+		{
+			if($row["cargo"] == 'CONTADOR')
+			{
+				session_start();
+				$_SESSION['usuario'] = $usuario;
+				header("Location: principal_contador.php");
+			}
+			else
+			{
+			  if($row["cargo"] == 'SECRETARIA')
+			{
+				session_start();
+				$_SESSION['usuario'] = $usuario;
+				header("Location: principal_secretaria.php");
+			}
+			else
+			{
+			  ?>
+				<script languaje="javascript">
+				alert("Se envio una notificacion al administrador");
+				location.href = "index.php";
+				</script>
+			  <?php
+			}
+			}
+		}
+	}
+	else
+	{
+	  ?>
+		<script languaje="javascript">
+		alert("Se envio una notificacion al administrador");
+		location.href = "index.php";
+		</script>
+	  <?php
+	}
+	$intentos=+1;
+}
+?>
